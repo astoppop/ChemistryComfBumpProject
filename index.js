1f4cc9f9c324bb26f941c7937a5bd74629b8fd67
astoppop <michealhuang7@gmail.com>
2022-10-14 21:05:42 -0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest();
-xhr.open("GET", 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/2244', false);
+xhr.open("GET", 'https://cors-proxy.htmldriven.com/?url=https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/2244', true);
 xhr.setRequestHeader("Accepted", "chemical/x-mdl-sdfile");
 xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 xhr.setRequestHeader("Content-Type", "chemical/x-mdl-sdfile");
